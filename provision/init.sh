f193b39cf6fdfcc062a1c7c4f6f4201b3898b58f
Pavel Stetina <github@shark.kom.cz>
2017-04-11 00:07:19 +0200

    Install php 7.1 and run composer

--- provision/init.sh
+++ provision/init.sh
@@ -1,7 +1,8 @@
-echo "deb http://packages.dotdeb.org jessie all" > /etc/apt/sources.list.d/dotbeb.list
-apt-key adv --keyserver keys.gnupg.net --recv-keys 89DF5277
+apt-get install -y apt-transport-https lsb-release ca-certificates
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 apt-get update
-apt-get install curl nginx php7.1-fpm
+apt-get install -y curl git nginx php7.1-fpm php7.1-zip unzip zip
 mkdir --mode=774 /var/log/www
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 cd /vagrant && composer install
