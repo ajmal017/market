eade42b937f0d3633945ae4cc7e2a1d9a12813aa
Pavel Stetina <github@shark.kom.cz>
2019-09-11 22:02:37 +0200

    Bugfix: insert exchanges and codes to DB

--- migration/02.sql
+++ migration/02.sql
@@ -11,25 +11,40 @@ ALTER TABLE exchange
 	ADD FOREIGN KEY (main_exchange_id) REFERENCES exchange,
 	ADD FOREIGN KEY (main_exchange_code) REFERENCES exchange_code (code);
 
+INSERT INTO exchange (name) VALUES
+	('ICE FUTURES U.S.'),
+	('CHICAGO BOARD OF TRADE'),
+	('CHICAGO MERCANTILE EXCHANGE'),
+	('NEW YORK MERCANTILE EXCHANGE'),
+	('COMMODITY EXCHANGE INC.'),
+	('KANSAS CITY BOARD OF TRADE')
+	ON CONFLICT DO NOTHING;
+
 INSERT INTO exchange_code (exchange_id, code) VALUES
 	((SELECT id FROM exchange WHERE name = 'ICE FUTURES U.S.'), 'ICE'),
 	((SELECT id FROM exchange WHERE name = 'CHICAGO BOARD OF TRADE'), 'CBOT'),
 	((SELECT id FROM exchange WHERE name = 'CHICAGO MERCANTILE EXCHANGE'), 'CME'),
 	((SELECT id FROM exchange WHERE name = 'NEW YORK MERCANTILE EXCHANGE'), 'NYMEX'),
 	((SELECT id FROM exchange WHERE name = 'COMMODITY EXCHANGE INC.'), 'COMEX'),
-	((SELECT id FROM exchange WHERE name = 'KANSAS CITY BOARD OF TRADE'), 'KCBT');
+	((SELECT id FROM exchange WHERE name = 'KANSAS CITY BOARD OF TRADE'), 'KCBT')
+	ON CONFLICT DO NOTHING;
 
-UPDATE exchange SET main_exchange_code = 'CME' WHERE name IN (
-	'CHICAGO BOARD OF TRADE',
-	'COMMODITY EXCHANGE INC.',
-	'NEW YORK MERCANTILE EXCHANGE'
-);
+UPDATE exchange SET
+	main_exchange_id = (SELECT exchange_id FROM exchange_code WHERE code = 'CME'),
+	main_exchange_code = 'CME'
+	WHERE name IN (
+		'CHICAGO BOARD OF TRADE',
+		'COMMODITY EXCHANGE INC.',
+		'NEW YORK MERCANTILE EXCHANGE'
+	);
 
 ALTER TABLE instrument
 	ALTER COLUMN code TYPE VARCHAR(6),
-	RENAME COLUMN code TO symbol,
 	ALTER COLUMN exchange_id SET NOT NULL,
 	ADD FOREIGN KEY (exchange_id) REFERENCES exchange;
+
+ALTER TABLE instrument
+	RENAME COLUMN code TO symbol;
 
 ALTER TABLE contract
 	DROP COLUMN code,
@@ -38,7 +53,7 @@ ALTER TABLE contract
 CREATE VIEW contract_code AS
 	SELECT contract.*,
 		(
-			SELECT instrument.code
+			SELECT instrument.symbol
 			FROM instrument
 			WHERE contract.instrument_id = instrument.id
 		) || (
